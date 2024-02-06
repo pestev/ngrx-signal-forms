b64064dd0ba627ce68a60f5261499be3faa45b22
pestev <pstevko.dev@gmail.com>
2024-02-06 09:53:25 +0100

    fixed paths to previous package

--- libs/ngrx-signal-forms-test/README.md
+++ libs/ngrx-signal-forms-test/README.md
@@ -1,7 +1,7 @@
-# ngrx-signal-forms
+# ngrx-signal-forms-test
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test ngrx-signal-forms` to execute the unit tests.
+Run `nx test ngrx-signal-forms-test` to execute the unit tests.
--- libs/ngrx-signal-forms-test/jest.config.ts
+++ libs/ngrx-signal-forms-test/jest.config.ts
@@ -1,22 +1,22 @@
 /* eslint-disable */
 export default {
-  displayName: 'ngrx-signal-forms',
+  displayName: 'ngrx-signal-forms-test',
   preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/libs/ngrx-signal-forms',
+  setupFilesAfterEnv: [ '<rootDir>/src/test-setup.ts' ],
+  coverageDirectory: '../../coverage/libs/ngrx-signal-forms-test',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transformIgnorePatterns: [ 'node_modules/(?!.*\\.mjs$)' ],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
+    'jest-preset-angular/build/serializers/html-comment'
+  ]
 };
